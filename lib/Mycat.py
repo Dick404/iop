462ec1679cb9a47a650f671641c9f3ff2a4661df
root <root@gophic.novalocal>
2016-06-06 17:49:31 +0800

    modified:   lib/Mycat.py

--- lib/Mycat.py
+++ lib/Mycat.py
@@ -14,7 +14,12 @@ class Mycat():
         return
 
     def install(self):
-        self.__Excute("tar -zxvf ../resource/cache/Mycat/Mycat-server-1.5-RELEASE-linux.tar.gz -C /usr/local/")
+        self.__Excute("tar -zxvf resource/cache/Mycat/Mycat-server-1.5-RELEASE-linux.tar.gz -C /usr/local/")
+        if os.path.exists("/usr/local/java"):
+            self.__Excute("tar -zxvf resource/cache/jdk/java.tar.gz -C /usr/local/java/")
+        else:
+            self.__Excute("mkdir /usr/local/java")
+            self.__Excute("tar -zxvf resource/cache/jdk/java.tar.gz -C /usr/local/java/")
         if os.path.exists("/usr/local/mycat/bin/mycat"):
             return True
         else :
@@ -22,13 +27,12 @@ class Mycat():
         return
 
     def config(self,ips):
-        tree = ET.parse('../config/schema.xml')
+        tree = ET.parse('config/schema.xml')
         root = tree.getroot()
         data_node = root.getiterator("dataHost")
         data_node = data_node[0]
         for i in range(len(ips)):
-            data_node.append("writeHost")
-            pass
+            data_node[0].append("writeHost")
         writeHost_node = data_node.findall("writeHost")
         for i in range(len(writeHost_node)):
             writeHost_node[i].set("host","hostM"+str(i))
@@ -40,7 +44,7 @@ class Mycat():
         return
 
     def run(self):
-        self.__Excute("/usr/local/mycat/bin/mycat start")
+        self.__Excute("export JAVA_HOME=/usr/local/java;export path=\$path\:\$JAVA_HOME/bin;/usr/local/mycat/bin/mycat start")
         if os.path.exists("/usr/local/mycat/logs/mycat.pid"):
             return True
         else :
