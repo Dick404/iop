c86638b9ed1f35b9f38b3496d897498022d426c3
liyongxin <liyongxin@inspur.com>
2016-06-10 18:44:28 +0800

    MySQL 相关

--- clean_mysql.sh
+++ clean_mysql.sh
@@ -1,2 +1,12 @@
-yum remove mariadb -y
-rm -rf /etc/my.cnf /etc/my.cnf.d/ /var/log/mariadb /var/lib/mysql
+yum remove -y MariaDB-Galera-server
+yum remove -y MariaDB-client 
+yum remove -y galera 
+yum remove -y rsync
+rpm -e --nodeps MariaDB-common-10.0.21-1.el7.centos.x86_64
+rpm -e --nodeps MariaDB-client-10.0.21-1.el7.centos.x86_64
+rpm -e --nodeps MariaDB-shared-10.0.21-1.el7.centos.x86_64
+rpm -e --nodeps MariaDB-Galera-server-10.0.21-1.el7.centos.x86_64
+
+rm -rf /var/lib/mysql 
+rm -rf /etc/my.cnf.d/wsrep.cnf
+rm -rf /etc/my.cnf.d/wsrep.cnf.bak
